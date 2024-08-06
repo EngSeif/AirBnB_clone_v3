b174516a0b3a53e7f86402b27a71d1c81fd1d6d3
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-08-06 05:57:31 +0300

    upload

---
+++ git.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Get a random description word
+if [ $# -ge 1 ];
+then
+        commit_word="$@"
+else
+        commit_word="upload"
+fi;
+
+touch test
+# Add all the files in the current directory to the Git staging area
+git add .
+
+# Create a new commit with the random description word
+git commit -m "$commit_word"
+
+# Push the changes to the remote repository
+git push
+
+rm test
